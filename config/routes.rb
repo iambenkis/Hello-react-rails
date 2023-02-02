556880e2d113ce84379daf2dceb36eb046f61657
benjamin kisenge <benkisenge03@gmail.com>
2023-02-03 01:25:34 +0200

    Add needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'greeting/index'
+  # get 'greeting/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
+  root to:'greetings#show'
 end
